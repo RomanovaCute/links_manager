d7cd43600c682416a2aa6b06338d68c8ac52246c
RomanovaCute <romanova17romanova@yandex.ru>
2025-08-18 12:02:23 +0500

    preparation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import '@/assets/styles/main.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +12,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 
 app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>Links Manager</main>
+  <Button label="Submit" />
+</template>
+
+<script setup>
+import Button from 'primevue/button'
+</script>
