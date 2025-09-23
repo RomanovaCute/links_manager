30ee17b2b6fe0edcd2f0af96fe99aea07546b34f
RomanovaCute <romanova17romanova@yandex.ru>
2025-09-23 13:14:21 +0500

    add tabs signin & signup

---
+++ src/components/AuthForm/AuthTabs.vue
@@ -0,0 +1,23 @@
+<template>
+  <Tabs v-model:value="activeTab">
+    <TabList>
+      <Tab value="signin">Вход</Tab>
+      <Tab value="signup">Регистрация</Tab>
+    </TabList>
+    <TabPanels>
+      <TabPanel value="signin">Вход контент</TabPanel>
+      <TabPanel value="signup">Регистрация контент</TabPanel>
+    </TabPanels>
+  </Tabs>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Tabs from 'primevue/tabs'
+import TabList from 'primevue/tablist'
+import Tab from 'primevue/tab'
+import TabPanels from 'primevue/tabpanels'
+import TabPanel from 'primevue/tabpanel'
+
+const activeTab = ref('signin') // signin | signup | reset
+</script>
---
+++ src/views/AuthView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center items-center min-h-screen">
+    <div class="p-6 bg-white shadow-lg rounded-lg w-full md:w-110 text-black dark:bg-white">
+      <AuthTabs />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AuthTabs from '@/components/AuthForm/AuthTabs.vue'
+</script>
